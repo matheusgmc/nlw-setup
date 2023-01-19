e655de9785b22c70f130ab98d3010bdebc0f3642
matheusgmc <sdmastergames@hotmail.com>
2023-01-18 20:11:40 -0400

    Aula - 2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
-import { Habit } from "./components/Habit";
+import "./styles/global.css";
+
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
+
 export function App() {
   return (
-    <>
-      <Habit completed={1} />
-      <Habit completed={2} />
-      <Habit completed={4} />
-    </>
+    <div className="w-scren h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   );
 }
