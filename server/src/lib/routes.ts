bdc04736cf86447188e7e8532144c1e4c9093d85
matheusgmc <sdmastergames@hotmail.com>
2023-01-20 20:11:36 -0400

    toggle Aula - 3

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -57,17 +57,65 @@ export async function appRoutes(app: FastifyInstance) {
         date: parseDate.toDate(),
       },
       include: {
-        dayHabits: {
-          select: {
-            id: true,
-          },
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((elem) => elem.habit_id);
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParam = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParam.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
         },
       },
     });
 
-    return {
-      possibleHabits,
-      completedHabits: day,
-    };
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
   });
+
+  app.get("/summary", async () => {});
 }
